50a107c81f37d810dad0803bff0e2f38d1901d39
mary-ue <maryandreeva100@gmail.com>
2024-01-10 00:02:45 +0300

    add initialState, loading and error for todos

--- src/store/todos/todos-actions.js
+++ src/store/todos/todos-actions.js
@@ -1,14 +1,28 @@
 import { client } from '../../api';
 
-export const ADD_TODOS = 'ADD_TODOS';
+export const ADD_TODOS = '@@todos/ADD_TODOS';
+export const SET_LOADING = '@@todos/SET_LOADING';
+export const SET_ERROR = '@@todos/SET_ERROR';
 
 const addTodos = (todos) => ({
   type: ADD_TODOS,
   payload: todos,
 });
 
+const setLoading = () => ({
+  type: SET_LOADING,
+});
+
+const setError = (error) => ({
+  type: SET_ERROR,
+  payload: error,
+});
+
 export const loadTodos = () => (dispatch) => {
-  client.get('https://jsonplaceholder.typicode.com/todos')
+  dispatch(setLoading());
+
+  client
+    .get('https://jsonplaceholder.typicode.com/todos')
     .then((todos) => dispatch(addTodos(todos)))
-    .catch((error) => console.error(error));
+    .catch((error) => dispatch(setError(error)));
 };
--- src/store/todos/todos-reducer.js
+++ src/store/todos/todos-reducer.js
@@ -1,9 +1,35 @@
-import { ADD_TODOS } from './todos-actions';
+import { ADD_TODOS, SET_ERROR, SET_LOADING } from './todos-actions';
 
-export const todosReducer = (state = [], action) => {
+const initialState = {
+  status: 'idle',
+  list: [],
+  error: null,
+}
+
+export const todosReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_TODOS: {
-      return action.payload;
+      return {
+        ...state,
+        list: action.payload,
+        status: 'fullfiled',
+      };
+    }
+
+    case SET_LOADING: {
+      return {
+        ...state,
+        status: 'loading',
+        error: null,
+      }
+    }
+
+    case SET_ERROR: {
+      return {
+        ...state,
+        status: 'rejected',
+        error: action.payload,
+      }
     }
 
     default: {
